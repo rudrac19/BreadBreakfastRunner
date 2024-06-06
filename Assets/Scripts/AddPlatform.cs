d5df368dc402cb2c5e6363952a5c4f2a6ff1e701
Rudra Pratap Chauhan <rcpython2@gmail.com>
2024-06-06 18:15:42 -0400

    Got the platform working!!
    (beta 1.0.17)

--- Assets/Scripts/AddPlatform.cs
+++ Assets/Scripts/AddPlatform.cs
@@ -4,23 +4,69 @@ using UnityEngine;
 
 public class AddPlatform : MonoBehaviour
 {
-    public GameObject forkPrefab;  //1
-    public GameObject spoonPrefab; //2
-    public GameObject handPrefab; //3
+    
+    public GameObject forkPrefab;  //0
+    public GameObject spoonPrefab; //1
+    public GameObject handPrefab; //2
 
-    void Start()
+    public Transform movingPlatform;
+
+    public MovePlatform movePlatform;
+
+    int Idk = 0;
+
+
+    void Awake()
     {
         int randomPlatform = Random.Range(0, 3);
         switch (randomPlatform)
         {
             case 0:
-                Instantiate(forkPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+                GameObject forkPlatform = Instantiate(forkPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+                forkPlatform.transform.SetParent(movingPlatform, false);
+                break;
             case 1:
-                Instantiate(spoonPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+                GameObject spoonPlatform = Instantiate(spoonPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+                spoonPlatform.transform.SetParent(movingPlatform, false);
+                break;
             case 2:
-                Instantiate(handPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+                GameObject handPlatform = Instantiate(handPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+                handPlatform.transform.SetParent(movingPlatform, false);
+                break;
             default:
                 break;
         }
+    }
+
+    void Start()
+    {
+        StartCoroutine(SpawnAndCooldown());
+    }
+
+    IEnumerator SpawnAndCooldown()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(5f);
+            int randomPlatform = Random.Range(0, 3);
+            switch (randomPlatform)
+            {
+                case 0:
+                    GameObject forkPlatform = Instantiate(forkPrefab, new Vector3((Idk += 15), 0, 0), Quaternion.identity);
+                    forkPlatform.transform.SetParent(movingPlatform, false);
+                    break;
+                case 1:
+                    GameObject spoonPlatform = Instantiate(spoonPrefab, new Vector3((Idk += 15), 0, 0), Quaternion.identity);
+                    spoonPlatform.transform.SetParent(movingPlatform, false);
+                    break;
+                case 2:
+                    GameObject handPlatform = Instantiate(handPrefab, new Vector3((Idk += 15), 0, 0), Quaternion.identity);
+                    handPlatform.transform.SetParent(movingPlatform, false);
+                    break;
+                default:
+                    break;
+            }
+        }
+        
     }
 }
